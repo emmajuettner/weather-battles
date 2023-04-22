656fa1ac035a6e3b30eb95ac4c745893e780d1a6
Emma Juettner <22248856+emmajuettner@users.noreply.github.com>
2023-04-22 13:41:30 -0500

    Add logic for saving poem lines

--- add_to_poem.py
+++ add_to_poem.py
@@ -5,8 +5,6 @@ import os
 from pathlib import Path
 from jinja2 import FileSystemLoader, Environment
 
-print("Calling the OpenWeather API")
-
 # Constants
 OPEN_WEATHER_API_KEY = os.environ['OPEN_WEATHER_API_KEY']
 LATITUDE = "41.890790"
@@ -16,16 +14,24 @@ OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%
 # Call OpenWeather API
 response = requests.get(OPEN_WEATHER_URL)
 data = json.loads(response.text)
+print("Response from the OpenWeather API:")
 print(data)
 
 # Pull out the specific weather stats we want to use
 weather = data["weather"][0]["main"]
-dt = str(data["dt"])
-
 print("weather="+weather)
-print("dt="+dt)
 
-# Build an html file based on the output we've generated
+# Construct the new poem line
+newLine = weather
+print("New line is: " + newLine)
+
+# Add the new poem line to the existing poem lines from the repo
+prevPoemLines = Path('poem_lines.txt').read_text()
+poem = prevPoemLines + "\n" + newLine
+Path("poem_lines.txt").write_text(poem)
+print("Updated poem_lines.txt")
+
+# Build an html file populated with the poem we've generated
 loader = FileSystemLoader(".")
 env = Environment(
     loader=loader, extensions=["jinja2_humanize_extension.HumanizeExtension"]
@@ -34,12 +40,8 @@ template = env.get_template("index.jinja")
 Path("index.html").write_text(
     template.render(
         {
-            "weather" : weather,
-            "date" : dt
+            "poem" : poem
         }
     )
 )
-
 print("Generated index.html")
-
-print("Finished running add_to_poem.py")
