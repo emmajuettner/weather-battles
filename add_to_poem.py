577639ee533ab6b825fe480319e4a190e77f6ff0
Emma Juettner <22248856+emmajuettner@users.noreply.github.com>
2023-04-22 12:45:26 -0500

    Get stats from api call to populate html page

--- add_to_poem.py
+++ add_to_poem.py
@@ -10,8 +10,30 @@ OPEN_WEATHER_API_KEY = os.environ['OPEN_WEATHER_API_KEY']
 LATITUDE = "41.890790"
 LONGITUDE = "-87.581239"
 OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s" % (LATITUDE, LONGITUDE, OPEN_WEATHER_API_KEY)
+
+# Call OpenWeather API
 response = requests.get(OPEN_WEATHER_URL)
 data = json.loads(response.text)
 print(data)
+
+# Pull out the specific weather stats we want to use
+weather = data["weather"][0]["main"]
+dt = data["dt"]
+
+print("weather="+weather)
+print("dt="+dt)
+
+# Build an html file based on the output we've generated
+template = env.get_template("index.jinja")
+Path("index.html").write_text(
+    template.render(
+        {
+            "weather" : weather,
+            "date" : dt
+        }
+    )
+)
+
+print("Generated index.html")
 
 print("Finished running add_to_poem.py")
