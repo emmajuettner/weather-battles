975e1e8cb215a9c4b46128c415f886303b2104ec
Emma Juettner <22248856+emmajuettner@users.noreply.github.com>
2023-04-22 11:28:38 -0500

    Add python script to call weather API

---
+++ add_to_poem.py
@@ -0,0 +1,12 @@
+print("Calling the OpenWeather API")
+
+# Constants
+OPEN_WEATHER_API_KEY = os.environ['OPEN_WEATHER_API_KEY']
+LATITUDE = "41.890790"
+LONGITUDE = "-87.581239"
+OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s" % (LATITUDE, LONGITUDE, OPEN_WEATHER_API_KEY)
+response = requests.get(OPEN_WEATHER_URL)
+data = json.loads(response.text)
+print(data)
+
+print("Finished running add_to_poem.py")
